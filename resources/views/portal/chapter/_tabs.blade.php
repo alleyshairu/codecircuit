e0d7c9729484e634be8d0d873ea11e999160bba3
alleyshairu <tfck.10@gmail.com>
2024-04-20 13:15:30 +1000

    fix tabs ui and show course tab in chapter pages

--- resources/views/portal/chapter/_tabs.blade.php
+++ resources/views/portal/chapter/_tabs.blade.php
@@ -1,8 +1,10 @@
-<div class="w-[400px] my-4">
-    <div role="tablist" class="tabs w-min">
+<div class="w-full my-4">
+    <div role="tablist" class="tabs grid-cols-3">
+        <a href="{{ route('portal.course.show', $chapter->language_id) }}" class="tab-item"
+            data-state="{{ request()->routeIs('portal.course.show') ? 'active' : '' }}">Course</a>
         <a href="{{ route('portal.chapter.edit', $chapter->id()) }}" class="tab-item"
-            data-state="{{ request()->routeIs('portal.chapter.edit') ? 'active' : '' }}">Edit Chapter</a>
+            data-state="{{ request()->routeIs('portal.chapter.edit') ? 'active' : '' }}">Chapter</a>
         <a href="{{ route('portal.chapter.problems', $chapter->id()) }}" class="tab-item"
-            data-state="{{ request()->routeIs('portal.chapter.problems') ? 'active' : '' }}">View Problems</a>
+            data-state="{{ request()->routeIs('portal.chapter.problems') ? 'active' : '' }}">Problems</a>
     </div>
 </div>
--- resources/views/portal/problem/_tabs.blade.php
+++ resources/views/portal/problem/_tabs.blade.php
@@ -1,5 +1,5 @@
-<div class="w-1/2 my-4">
-    <div role="tablist" class="tabs w-min">
+<div class="w-full my-4">
+    <div role="tablist" class="tabs grid-cols-2">
         <a href="{{ route('portal.problem.edit', $problem->id()) }}" class="tab-item"
             data-state="{{ request()->routeIs('portal.problem.edit') ? 'active' : '' }}">Edit Problem</a>
     </div>
